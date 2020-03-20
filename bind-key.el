75d9a45b07a32d8a8bcc4577f270497ee84348da
Wang Chunye <chunywan@xilinx.com>
2020-03-20 06:25:35 -0700

    optimization: constand folding for read-kbd-macro
    to boost startup performance, it is better to avoid invoking
    `read-kbd-macro` at run time which requires 'cl-lib.
    
    it takes ~20ms to load cl-lib

--- bind-key.el
+++ bind-key.el
@@ -169,8 +169,10 @@ can safely be called at any time."
         (kdescvar (make-symbol "kdesc"))
         (bindingvar (make-symbol "binding")))
     `(let* ((,namevar ,key-name)
-            (,keyvar (if (vectorp ,namevar) ,namevar
-                       (read-kbd-macro ,namevar)))
+            (,keyvar ,(if (stringp key-name)
+                          (read-kbd-macro key-name)
+                        `(if (vectorp ,namevar) ,namevar
+                          (read-kbd-macro ,namevar))))
             (,kdescvar (cons (if (stringp ,namevar) ,namevar
                                (key-description ,namevar))
                              (quote ,keymap)))
